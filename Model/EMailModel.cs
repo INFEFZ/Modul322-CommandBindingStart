c5a33300bc4374387b9349edbe7d1ed07a579ed3
Lukas Müller <8344295+KETA@users.noreply.github.com>
2022-11-28 07:31:04 +0100

    Add project files.

---
+++ Model/EMailModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Sample3.Model
+{
+    public class EMailModel
+    {
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Sample3.Model;
+using Sample3.Utility;
+using System.Windows;
+
+namespace Sample3.ViewModel
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private RelayCommand _cmdPrint;
+        private EMailModel _model = new EMailModel();
+
+        public MainWindowViewModel()
+        {
+            // TODO
+        }
+
+        //  TODO CmdPrint Property
+
+        public string ContentText
+        {
+            get { return _model.Content; }
+            set
+            {
+                _model.Content = value;
+                OnPropertyChanged("ContentText");
+            }
+        }
+
+        private void Execute_Print()
+        {
+            MessageBox.Show("Dokument wird gedruckt.", "Drucken");
+            ContentText = "";
+        }
+
+        private bool CanExecute_Print()
+        {
+             return ContentText != "";
+        }
+    }
+}
